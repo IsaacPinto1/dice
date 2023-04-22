1092d02c3308592fe331f783e10a054cc180714c
Isaac Pinto <isaac.pinto2003@gmail.com>
2023-04-21 20:12:57 -0700

    Initial Commit

---
+++ dice/diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceApp.swift
+//  dice
+//
+//  Created by Isaac Pinto on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
