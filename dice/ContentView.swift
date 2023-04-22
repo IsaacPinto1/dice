e232dec481ee947442cfeeef4dd559174bdcb431
Isaac Pinto <isaac.pinto2003@gmail.com>
2023-04-22 01:13:06 -0700

    commit 1

--- dice/ContentView.swift
+++ dice/ContentView.swift
@@ -8,14 +8,129 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isVisible = false
+    
+    func SignIn(){
+        isVisible = true
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            if isVisible == true {
+                TabView{
+                    SecondView()
+                        .tabItem{
+                            Label("Second", systemImage: "tray.and.arrow.down.fill")
+                        }
+                    ThirdView()
+                        .tabItem{
+                            Label("Third", systemImage: "tray.and.arrow.down.fill")
+                        }
+                }
+            } else {
+                VStack {
+                    Text("Not Signed in!")
+                    Button("Sign in") {
+                        SignIn()
+                    }
+                }
+                
+            }
+        
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        VStack{
+            VStack {
+                Text("This is the second view")
+            }
+            NavigationView{
+                NavigationLink(destination: FourthView()) { Text("Go to fourth") }
+                    .frame(width: 100.0, height: 100.0)
+            }
+            .padding(0.0)
+            .frame(width: 400.0, height: 200.0)
+        }
+    }
+}
+
+struct ThirdView: View {
+    var body: some View {
+        Text("This is third view")
+            .navigationBarTitle("ThirdView", displayMode: .inline)
+    }
+}
+
+struct FourthView: View{
+    var body: some View{
+        Text("Fourth View")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+        }
+    }
+}
+
+
+/*import Foundation
+import SwiftUI
+struct ContentView: View {
+    @State private var joke = Current
+    
+    
+    var body: some View {
+        Text("Hello")
+        Button{
+            Task {
+                let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")!
+                let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    guard let data = data else {
+                        print("Error: No data")
+                        return
+                    }
+                    do {
+                        let current = try JSONDecoder().decode(Current.self, from: data)
+                        print(current.currentWeather.temperature)
+                    } catch {
+                        print("Error decoding JSON: \(error)")
+                    }
+                }
+                task.resume()
+            }
+        } label: {
+            Text("Fetch")
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
         ContentView()
     }
 }
+
+
+
+struct Current: Codable {
+    let currentWeather: CurrentWeather
+    
+    private enum CodingKeys: String, CodingKey {
+        case currentWeather = "current_weather"
+    }
+}
+
+struct CurrentWeather: Codable {
+    let temperature: Double
+    let windspeed: Double
+    let winddirection: Double
+    let weathercode: Int
+    let is_day: Int
+    let time: String
+}*/
